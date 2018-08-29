a551f18d4bf33639c7b538f9cc1bb70a2053e0cd
JP Lynch <JPLynch35@gmail.com>
2018-08-29 09:41:54 -0600

    put correct URI in faraday request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
-    
+    conn = Faraday.new(url: "https://developer.nrel.gov")
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=ENV['TRANSPORT_KEY']=params[:q]")
+    @stations = JSON.parse(response.body)
   end
 end
