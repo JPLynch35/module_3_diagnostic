28bcdf85403d9a4b768040c0c961f053b546ec9c
JP Lynch <JPLynch35@gmail.com>
2018-08-29 09:20:24 -0600

    add first portion of ffeature test and orderly gem

---
+++ spec/features/user_can_see_root_page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'a user' do
+  describe 'visiting the root page' do
+    it 'can locate an area code' do
+      visit '/'
+      fill_in :q, with: '80203'
+      click_on 'Locate'
+
+      expect(current_path).to eq('/search')
+      expect(page).to have_css('stations', 10)
+      expect(page).to have_content()
+      expect(page)
+      within('station-1') do
+        expect(page).to have_content('Name:')
+        expect(page).to have_content('Address:')
+        expect(page).to have_content('Fuel Types:')
+        expect(page).to have_content('Distance:')
+        expect(page).to have_content('Access:')
+      end
+    end
+  end
+end
