c1e733c8954967990783b7ab738947265b759fe8
JP Lynch <JPLynch35@gmail.com>
2018-08-29 09:54:36 -0600

    add parameters to API get request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     conn = Faraday.new(url: "https://developer.nrel.gov")
-    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=ENV['TRANSPORT_KEY']=params[:q]")
-    @stations = JSON.parse(response.body)
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['TRANSPORT_KEY']}&location=params[:q]&fuel_type=ELEC,LPG&limit=10&radius=6")
+    unsorted_stations = JSON.parse(response.body)
   end
 end
