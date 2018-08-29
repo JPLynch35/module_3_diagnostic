d9792409ac7db87051524640cbee1d69e52cba32
JP Lynch <JPLynch35@gmail.com>
2018-08-29 10:07:56 -0600

    fix api request link

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,10 @@
 class SearchController < ApplicationController
   def index
     conn = Faraday.new(url: "https://developer.nrel.gov")
-    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['TRANSPORT_KEY']}&location=params[:q]&fuel_type=ELEC,LPG&limit=10&radius=6")
-    unsorted_stations = JSON.parse(response.body)
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['TRANSPORT_KEY']}&location=#{params[:q]}&fuel_type=ELEC,LPG&limit=10&radius=6")
+    unsorted_stations = JSON.parse(response.body, symbolize_names: true)
+    @stations = unsorted_stations[:fuel_stations].map do |fuel_station|
+      Station.new(fuel_station)
+    end
   end
 end
